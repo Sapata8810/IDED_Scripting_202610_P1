568e7b38a2b2393c7675d4c29e3d7f0edb83971e
mafora12 <marianaflorez1224@gmail.com>
2026-02-16 22:45:44 -0500

    final_pruebas

--- TestMethods.cs
+++ TestMethods.cs
@@ -7,41 +7,37 @@
             included = new Stack<int>();
             excluded = new Stack<int>();
 
-            Stack<int> tempIncluded = new Stack<int>();
-            Stack<int> tempExcluded = new Stack<int>();
-
             while (input.Count > 0)
             {
                 int value = input.Dequeue();
-
                 int abs = value < 0 ? -value : value;
-                int root = 1;
 
+                int root = 0;
                 while (root * root < abs)
                 {
                     root++;
                 }
 
-                bool isPerfectSquare = root * root == abs;
+                bool isPerfectSquare = (root * root == abs);
 
                 if (isPerfectSquare)
                 {
-                    if ((root % 2 == 1 && value < 0) || (root % 2 == 0 && value > 0))
-                        tempIncluded.Push(value);
+                
+                    if ((root % 2 == 0 && value >= 0) || (root % 2 != 0 && value < 0))
+                    {
+        
+                        included.Push(value);
+                    }
                     else
-                        tempExcluded.Push(value);
+                    {
+                        excluded.Push(value);
+                    }
                 }
                 else
                 {
-                    tempExcluded.Push(value);
+                    excluded.Push(value);
                 }
             }
-
-            while (tempIncluded.Count > 0)
-                included.Push(tempIncluded.Pop());
-
-            while (tempExcluded.Count > 0)
-                excluded.Push(tempExcluded.Pop());
         }
 
         public static List<int> GenerateSortedSeries(int n)
